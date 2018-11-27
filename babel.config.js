3aa9c7ec2b714801cf96fe894e8adb1092445232
xuyimingwork <xuyimingwork@foxmail.com>
2018-11-27 16:29:36 +0800

    设为私有

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,14 @@
 module.exports = {
   "presets": [
-    ["@babel/preset-env", {
-      "modules": false,
-      "targets": {
-        "browsers": ["> 1%", "last 2 versions", "not ie <= 8"]
+    [
+      "@babel/preset-env",
+      {
+        "modules": false,
+        "targets": {
+          "browsers": ["> 1%", "last 2 versions", "not ie <= 8"]
+        }
       }
-    }]
+    ]
   ],
   "plugins": [
     "transform-vue-jsx",
