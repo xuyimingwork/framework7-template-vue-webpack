a6f83b9380af891de1fbc2eddfcc2141787e952a
xuyimingwork <xuyimingwork@foxmail.com>
2018-11-27 16:39:15 +0800

    样式文件位置变动

--- src/app.js
+++ src/app.js
@@ -4,15 +4,15 @@ import Vue from 'vue';
 // Import F7
 import Framework7 from 'framework7/framework7.esm.bundle.js';
 
-// Import F7 Vue Plugin
+// Import F7 Vue Plugin -- 引入全部 f7 组件
 import Framework7Vue from 'framework7-vue/framework7-vue.esm.bundle.js';
 
 // Import F7 Styles
 import Framework7Styles from 'framework7/css/framework7.css';
 
 // Import Icons and App Custom Styles
-import IconsStyles from './css/icons.css';
-import AppStyles from './css/app.css';
+import IconsStyles from './style/css/icons.css';
+import AppStyles from './style/css/app.css';
 
 // Import App Component
 import App from './app.vue';
