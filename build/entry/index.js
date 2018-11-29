75b9465f978f8463ea2a0c1e90345c1bbdf8a29d
xuyimingwork <xuyimingwork@foxmail.com>
2018-11-29 14:01:12 +0800

    优化构建输出

--- build/entry/index.js
+++ build/entry/index.js
@@ -11,22 +11,28 @@ const i18n = {
   'production': '生产',
 };
 
+const getOraText = function(str) {
+  return `${chalk.cyan(str)}`;
+};
+
 function run(webpackConfig) {
   const versionName = i18n[webpackConfig.mode];
-  const spinner = ora(`正在构建${versionName}版本...`)
-  spinner.start();
+  const spinner = ora();
+  spinner.start(getOraText('初始化...'));
 
   const projectDir = path.resolve('.');
   const outputDir = path.resolve(webpackConfig.output.path);
-  const relativeOutputDir = `./${path.relative(projectDir, outputDir).replace('\\', '/')}/`
+  const relativeOutputDir = `./${path.relative(projectDir, outputDir).replace('\\', '/')}/`;
   
+  spinner.text = getOraText('正在清理目标文件夹...');
   rm(relativeOutputDir, (removeErr) => {
     if (removeErr) throw removeErr;
-  
+    
+    spinner.text = getOraText(`正在构建${versionName}版本...`);
     webpack(webpackConfig, (err, stats) => {
       if (err) throw err;
-      spinner.stop();
-  
+      spinner.clear();
+      
       process.stdout.write(stats.toString({
         colors: true,
         modules: false,
@@ -36,11 +42,11 @@ function run(webpackConfig) {
       }) + '\n\n');
   
       if (stats.hasErrors()) {
-        console.log(chalk.red('出现错误，构建失败.\n'));
+        spinner.fail(chalk.red('出现错误，构建失败.'));
         process.exit(1);
       }
   
-      console.log(chalk.cyan(`${versionName}版本构建完成.\n`));
+      spinner.succeed(chalk.cyan(`${versionName}版本构建完成.`));
     });
   });
 }
